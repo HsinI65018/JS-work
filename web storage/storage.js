f9c9ad9a629a3189efed243798ad0f6d8dbef589
Chang Hsin-I <93517414+HsinI65018@users.noreply.github.com>
2021-12-08 19:04:12 +0800

    Update storage.js

--- web storage/storage.js
+++ web storage/storage.js
@@ -6,35 +6,34 @@ var bgcolorForm = document.getElementById('bgcolor');
 var fontForm = document.getElementById('font');
 var imageForm = document.getElementById('image');
 
-if (!localStorage.getItem('bgcolor')) {
-	populateStorage() //testing to see whether the bgcolor item exists; if not, we run populateStorage()
-}else{
-	setStyles()
+if(!localStorage.getItem('bgcolor')) {
+  populateStorage();
+} else {
+  setStyles();
+}
+
+function populateStorage() {
+  localStorage.setItem('bgcolor', document.getElementById('bgcolor').value);
+  localStorage.setItem('font', document.getElementById('font').value);
+  localStorage.setItem('image', document.getElementById('image').value);
+
+  setStyles();
 }
 
 function setStyles() {
-	var currentColor = localStorage.getItem('bgcolor');
-	var currentFont = localStorage.getItem('font');
-	var currentImage = localStorage.getItem('image');
+  var currentColor = localStorage.getItem('bgcolor');
+  var currentFont = localStorage.getItem('font');
+  var currentImage = localStorage.getItem('image');
 
-	bgcolorForm.value = currentColor;
-	fontForm.value = currentFont;
-	imageForm.value = currentImage;
+  document.getElementById('bgcolor').value = currentColor;
+  document.getElementById('font').value = currentFont;
+  document.getElementById('image').value = currentImage;
 
-	htmlElem.style.backgroundColor = '#' + currentColor;
-	pElem.style.fontFamily = currentFont;
-	imgElem.setAttribute('src',currentImage);
-}
-
-function populateStorage(){
-	localStorage.setItem('bgcolor',bgcolorForm);
-	localStorage.setItem('font',fontForm);
-	localStorage.setItem('image',imageForm);
-
-	setStyles();
+  htmlElem.style.backgroundColor = '#' + currentColor;
+  pElem.style.fontFamily = currentFont;
+  imgElem.setAttribute('src', currentImage);
 }
 
 bgcolorForm.onchange = populateStorage;
 fontForm.onchange = populateStorage;
 imageForm.onchange = populateStorage;
-
